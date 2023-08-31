29d58069e2d79f88350450afa2bb3bacf3fe516e
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-08-31 22:37:34 +0100

    updating file 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 
 
-/**
- * binary_tree_levelorder -  this function goes through a binary tree using
- * level-order traversal
- * @tree: a pointer to the root node of the tree to traverse
- * @func: a pointer to a function to call for each node
- */
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
-{
-	int len, i;
-	int *tree_arr;
-
-	if (!tree || !func)
-		return;
-	len = binary_tree_size(tree);
-	tree_arr = calloc(size, sizeof(int));
-	saver(tree, tree_arr, 0);
-	for (i = 0; i < len; i++)
-		func(tree_arr[i]);
-	free(tree_arr);
-}
 
 /**
  * binary_tree_size - calculates the size of a tree
@@ -30,7 +10,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
@@ -51,4 +31,25 @@ void saver(const binary_tree_t *tree, int *tree_arr, int index)
 		saver(tree->left, tree_arr, (2 * index) + 1);
 	if (tree->right)
 		saver(tree->right, tree_arr, (2 * index) + 2);
+}
+
+/**
+ * binary_tree_levelorder - this function that goes through a binary tree
+ * using level-order traversal
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	int size, i;
+	int *tree_arr;
+
+	if (!tree || !func)
+		return;
+	size = binary_tree_size(tree);
+	tree_arr = calloc(size, sizeof(int));
+	saver(tree, tree_arr, 0);
+	for (i = 0; i < size; i++)
+		func(tree_arr[i]);
+	free(tree_arr);
 }
