47bebc78b594769d1597fd19d912ef2bd170e89f
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-09-01 00:39:46 +0100

    updating task 120

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -2,20 +2,6 @@
 #include <limits.h>
 #include <math.h>
 
-/**
- * binary_tree_is_bst - check if bt is bst
- * @tree: tree
- * Return: 0 or 1
- */
-int binary_tree_is_bst(const binary_tree_t *tree)
-{
-	int prev;
-
-	if (!tree)
-		return (0);
-	prev = INT_MIN;
-	return (inorder(tree, &prev));
-}
 
 /**
  * inorder - helper
@@ -33,6 +19,21 @@ int inorder(const binary_tree_t *tree, int *prev)
 		return (0);
 	*prev = tree->n;
 	return (inorder(tree->right, prev));
+}
+
+/**
+ * binary_tree_is_bst - check if bt is bst
+ * @tree: tree
+ * Return: 0 or 1
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	int prev;
+
+	if (!tree)
+		return (0);
+	prev = INT_MIN;
+	return (inorder(tree, &prev));
 }
 
 /**
